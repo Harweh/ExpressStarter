9c46ead8c83c6300f4c82a52f77654a77b84802e
Emmy <e.awetobi@gmail.com>
2025-07-29 11:40:16 +0100

    some backend code

--- server.js
+++ server.js
@@ -44,15 +44,118 @@ app.listen(3000, () => {
 // body
 // how to write a post request
 
-app.post( '/', (req, res) => {
-    const { email} = req.body;
-    res.send(`Password: 1234 for ${ email } `)
+// app.post( '/', (req, res) => {
+//     const { email} = req.body;
+//     res.send(`Password: 1234 for ${ email } `)
+// })
+
+// app.listen(3000, () => {
+//     console.log('Server is live on http://localhost:3000');
+    
+// })
+
+
+// //Database
+// const users = [
+//     {
+//         email : 'Emmy@gmail.com',
+//         password : 'password'
+//     },
+
+//      {
+//         email : 'Awe@gmail.com',
+//         password : 'password1'
+//     }
+// ]
+
+// app.post('/Login', (req, res) => {
+//      const { email, password } = req.body
+
+
+//      users.forEach( user => {
+//         if (email === user.email){
+//             if ( password === user.password){
+//                 return res.status(200).json({
+//                     success: true,
+//                     message: 'Login Successful'
+//                 })
+//             }
+//             return res.status(400).json({
+//                 success: false,
+//                 message: 'Password is wrong'
+//             })
+//         }
+         
+//      })
+//         return res.status(400).json({
+//                 success : false,
+//                 message : 'Email is wrong'
+//             })
+// })
+
+
+// app.listen(3000, () => {
+//     console.log('Server is live on http://localhost:3000');
+    
+// })
+
+
+
+// 2nd/july/2025
+
+app.post('/Login', (req, res)=> {
+    try {
+
+        const {email, password} = req.body
+
+        console.log(!email || !password);
+    
+    
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: 'invalid email or password'
+            })
+        }
+
+
+         const user = users.find (user => user.email === email)
+
+        if(!user) {
+            return res.status(400).json({
+                success: false,
+                message: 'Email not found or wrong'
+            })
+        }
+
+        if(user.password !== password) {
+                return res.status(400).json({
+                success : false,
+                message : 'Password is wrong'
+            })
+        }
+
+        return res.status(200).json({
+            success : true,
+            message : 'Login succeessful'
+        })
+            
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong'
+        })
+    }
 })
+
 
 app.listen(3000, () => {
     console.log('Server is live on http://localhost:3000');
     
 })
+
+
 
 
 //Database
@@ -68,29 +171,45 @@ const users = [
     }
 ]
 
-app.post('/Login', (req, res) => {
-     const { email, password } = req.body
 
+app.post('/Signup', (req, res) => {
 
-     users.forEach( user => {
-        if (email === user.email){
-            if ( password === user.password){
-                return res.status(200).json({
-                    success: true,
-                    message: 'Login Successful'
-                })
-            }
-            return res.status(400).json({
-                success: false,
-                message: 'Password is wrong'
-            })
-        }
-     })
-        return res.status(400).json({
-                success : false,
-                message : 'Email is wrong'
-            })
+    try {
+
+        const { email, password } = req.body
+
+        users.push({
+            'email' : email,
+            'password' : password
+        })
+
+        res.status(200).json({
+            success : true,
+            'users' : users
+        })
+        
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong'
+        })
+    }
 })
+
+
+
+
+//write a get request to fetch all the users
+app.get('/users', (req, res)=>{
+    res.status(200).json({
+        "x" : users
+    })
+})
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Server is live on http://localhost:3000');
